80e3e9e372a9f33e9a5eaede132f705b5dc0ec3f
Quentin Anthony <qganthony@yahoo.com>
2022-11-11 09:47:11 -0500

    Add CUDA 11.4 to ease the switch to newer CUDA

--- modules/43.allow.nvidia.debug.gpu.sh
+++ modules/43.allow.nvidia.debug.gpu.sh
@@ -17,7 +17,7 @@ installDCGM() {
 
 bumpUp(){
     yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
-    yum -y install cuda-toolkit-11-6 libcudnn8 libcudnn8-devel
+    yum -y install cuda-toolkit-11-6 cuda-toolkit-11-4 libcudnn8 libcudnn8-devel
 }
 
 installCudaModules(){
